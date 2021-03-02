b267a0e7a367c29d578067ffa1160904370a61da
jonaskienreich <71692978+jonaskienreich@users.noreply.github.com>
2021-03-02 09:15:01 +0100

    Update test_RGB.py

--- test_RGB.py
+++ test_RGB.py
@@ -2,21 +2,21 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-blue = 11
-red = 12
-green = 13
-button = 10
-GPIO.setup(blue, GPIO.OUT) #Blue LED
-GPIO.setup(red, GPIO.OUT) #Red LED
+blue = 11           # blue LED is connected to Pin 11
+red = 12            # red LED is connected to Pin 12
+green = 13          # green LED is connected to Pin 13
+button = 10         # Button is connected to Pin 14
+GPIO.setup(blue, GPIO.OUT)  #Blue LED
+GPIO.setup(red, GPIO.OUT)   #Red LED
 GPIO.setup(green, GPIO.OUT) #Green LED
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP) #button PD
 
 colors = [0xFF0000, 0x0F0F00, 0x00FF00]
-pwm_red = GPIO.PWM(red, 5000) #PWM mit Frequenz 2kHz erstellen
+pwm_red = GPIO.PWM(red, 5000)   #PWM with Frequency of 2kHz
 pwm_blue = GPIO.PWM(blue, 5000)
 pwm_green = GPIO.PWM(green, 5000)
 
-pwm_red.start(0) # TV auf 0 setzen
+pwm_red.start(0)                # duty cycle set 0
 pwm_blue.start(0)
 pwm_green.start(0)
 
@@ -37,10 +37,10 @@ def setColor(color):
 
 i = 0
 while True:
-    if GPIO.input(button) == GPIO.LOW: # push-buton pressed
-        setColor(colors[i]) #set color 
-        i+=1 # increment i by 1
-        if(i>=3): #reset i to 0
+    if GPIO.input(button) == GPIO.LOW:  # push-buton pressed
+        setColor(colors[i])             #set color 
+        i+=1                            # increment i by 1
+        if(i>=3):                       #reset i to 0
             i=0
-        time.sleep(0.1) # no keybounce
+        time.sleep(0.1)                 #no keybounce
     
